e8abafbc879d0ed834408bfd5403daaf991186b9
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2021-04-30 16:03:58 -0300

    Organizing code

---
+++ src/pdf.js
@@ -0,0 +1,105 @@
+const puppeteer = require('puppeteer');
+let browser;
+
+async function startBrowser() {
+    console.log("Starting headless browser");
+
+    browser = await puppeteer.launch({
+        devtools: false,
+        headless: true
+    });
+}
+
+function parseOptions(params) {
+    var options = { format: 'A4' };
+
+    try {
+        console.log('Parsing options');
+
+        for (var key in params) {
+            if (key !== 'encoding' && key !== 'url') {
+
+                // Parsing types
+                if (params[key] === 'true' || params[key] === 'false') {
+                    options[key] = (params[key] === 'true');
+                } else if (parseInt(params[key]) != NaN) {
+                    options[key] = parseInt(params[key]);
+                } else {
+                    options[key] = params[key];
+                }
+            }
+        }
+    } catch (e) {
+        console.error('Error while parsing options from request body');
+
+        throw e;
+    }
+
+    return options;
+};
+
+async function fromHtmlFile(file, body) {
+    console.log('Converting ' + file.originalname + ' to pdf');
+
+    try {
+        const options = parseOptions(body);
+
+        const html = Buffer.from(file.buffer).toString(body.encoding);
+        const page = await browser.newPage();
+        await page.setContent(html, { waitUntil: "networkidle0" });
+        const pdf = await page.pdf(options);
+        await page.close();
+
+        console.info(file.originalname + ' converted successfully');
+        return pdf;
+    } catch (e) {
+        throw e;
+    }
+};
+
+async function fromHtmlString(body) {
+    console.log('Converting HTML string to pdf');
+
+    try {
+        const html = body.html;
+        if (!html) {
+            throw {'message' : 'HTML text not found'}
+        }
+        const options = parseOptions(body);
+
+        const page = await browser.newPage();
+        await page.setContent(html, { waitUntil: "networkidle0" });
+        const pdf = await page.pdf(options);
+        await page.close();
+
+        console.info('String converted successfully');
+        return pdf;
+    } catch (e) {
+        throw e;
+    }
+};
+
+async function fromUrl(body) {
+    console.log('Converting ' + body.url + ' to pdf');
+
+    try {
+        const url = body.url;
+        if (!url) {
+            throw {'message' : 'Param URL not found'}
+        }
+
+        const options = parseOptions(body);
+
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: "networkidle0" });
+        const pdf = await page.pdf(options);
+        await page.close();
+
+        console.info('URL converted successfully');
+        return pdf;
+    } catch (e) {
+        throw e;
+    }
+};
+
+module.exports = { startBrowser, fromHtmlFile, fromHtmlString, fromUrl };
--- src/routes.js
+++ src/routes.js
@@ -1,49 +1,42 @@
 const express = require('express');
 const multer = require('multer');
-const puppeteer = require('puppeteer');
 
 const routes = express.Router();
 const upload = multer();
+const pdf = require('./pdf');
+pdf.startBrowser();
 
-routes.get("/htmltopdf", upload.single('anexo'), async (request, response) => {
+routes.post("/convert/fromFile", upload.single('file'), async (request, response) => {
     try {
-        // Loading input file with proper encoding
-        const encoding = (request.body.encoding) ? request.body.encoding : 'utf8';
-        const html = Buffer.from(request.file.buffer).toString(encoding);
-        console.log(html)
+        const result = await pdf.fromHtmlFile(request.file, request.body);
         
-        var defaultOptions = {format: 'A4'};
-        for (var key in request.body) {
-            // Ignoring enconding
-            if (key !== 'encoding') {
-
-                // Parsing types
-                if (request.body[key] === 'true' || request.body[key] === 'false') {
-                    defaultOptions[key] = (request.body[key] === 'true');
-                } else if (parseInt(request.body[key]) != NaN) {
-                    defaultOptions[key] = parseInt(request.body[key]);
-                } else {
-                    defaultOptions[key] = request.body[key];
-                }
-            }
-        }
-
-        // Initialing Headless browser
-        const browser = await puppeteer.launch({
-            devtools: false,
-            headless: true
-        });
-
-        // Generating PDF file
-        const page = await browser.newPage();
-        await page.setContent(html);
-        const pdf = await page.pdf(defaultOptions);
-        await page.close();
-
-        // Sending pdf back to the caller
         response.contentType("application/pdf");
-        response.status(201).send(pdf);
+        response.status(201).send(result);
     } catch (e) {
+        response.status(400).send(e);
+    }
+});
+
+routes.post("/convert/fromString", upload.none(), async (request, response) => {
+    try {
+        const result = await pdf.fromHtmlString(request.body);
+        
+        response.contentType("application/pdf");
+        response.status(201).send(result);
+    } catch (e) {
+        response.status(400).send(e);
+    }
+});
+
+routes.post("/convert/fromUrl", upload.none(), async (request, response) => {
+    try {
+        const result = await pdf.fromUrl(request.body);
+        
+        response.contentType("application/pdf");
+        response.status(201).send(result);
+    } catch (e) {
+        console.error(e);
+
         response.status(400).send(e);
     }
 });
