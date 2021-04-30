33896ba50c30d77ab426f11f575f15775bbf50ad
Gustavo Fragoso <gustavo-gbf@overmind.ai>
2021-04-30 14:44:45 -0300

    Fully working example

--- src/routes.js
+++ src/routes.js
@@ -7,18 +7,41 @@ const upload = multer();
 
 routes.get("/htmltopdf", upload.single('anexo'), async (request, response) => {
     try {
-        const content = Buffer.from(request.file.buffer).toString();
+        // Loading input file with proper encoding
+        const encoding = (request.body.encoding) ? request.body.encoding : 'utf8';
+        const html = Buffer.from(request.file.buffer).toString(encoding);
+        console.log(html)
+        
+        var defaultOptions = {format: 'A4'};
+        for (var key in request.body) {
+            // Ignoring enconding
+            if (key !== 'encoding') {
 
+                // Parsing types
+                if (request.body[key] === 'true' || request.body[key] === 'false') {
+                    defaultOptions[key] = (request.body[key] === 'true');
+                } else if (parseInt(request.body[key]) != NaN) {
+                    defaultOptions[key] = parseInt(request.body[key]);
+                } else {
+                    defaultOptions[key] = request.body[key];
+                }
+            }
+        }
+
+        // Initialing Headless browser
         const browser = await puppeteer.launch({
             devtools: false,
             headless: true
         });
 
+        // Generating PDF file
         const page = await browser.newPage();
-        await page.setContent(content);
-        const pdf = await page.pdf({format: 'A4'});
+        await page.setContent(html);
+        const pdf = await page.pdf(defaultOptions);
         await page.close();
 
+        // Sending pdf back to the caller
+        response.contentType("application/pdf");
         response.status(201).send(pdf);
     } catch (e) {
         response.status(400).send(e);
